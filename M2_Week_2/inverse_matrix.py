02df402583b66d05c2fce7707b9a5ab0caabe5b2
Kuuhakuu <kuuhakuu@Kuuhakuus-MacBook-Pro.local>
2024-07-22 22:29:42 +0700

    <Exercise M2W2>[Add]: <Complete Exercise M2W2>

---
+++ M2_Week_2/inverse_matrix.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def inverse_matrix(matrix):
+    result = np.linalg.inv(matrix)
+    return result
---
+++ M2_Week_2/matrix_multi_vector.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def matrix_multi_vector(matrix, vector):
+    result = np.dot(matrix, vector)
+    return result
