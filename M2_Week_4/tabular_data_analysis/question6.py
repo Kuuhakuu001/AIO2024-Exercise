7e5f9551f20f4cbb4c1bf036bd411973691b5bc1
Kuuhakuu <kuuhakuuv008@gmail.com>
2024-08-03 17:37:34 +0700

    <Exercise M2W4>[Modifiy]: <Modify duplication code>

--- M2_Week_4/tabular_data_analysis/question6.py
+++ M2_Week_4/tabular_data_analysis/question6.py
@@ -1,7 +1,16 @@
 # Question 6
 import numpy as np
 import pandas as pd
-from compute_correlation_cofficient import compute_correlation_cofficient
+
+
+def compute_correlation_cofficient (X , Y ):
+    N = len(X)
+    numerator = 0
+    denominator = 0
+    numerator = N * np.sum(X * Y) - np.sum(X) * np.sum(Y)
+    denominator = np.sqrt(N * np.sum(X**2) - np.sum(X)**2) * np.sqrt(N * np.sum(Y**2) - np.sum(Y)**2)
+    return np.round(numerator / denominator , 2)
+
 
 data = pd.read_csv ("advertising.csv")
 
