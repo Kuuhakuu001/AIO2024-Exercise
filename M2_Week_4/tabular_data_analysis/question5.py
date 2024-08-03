b241708613815fcc725fec7dedd39b52d9bfac81
Kuuhakuu <kuuhakuuv008@gmail.com>
2024-08-03 17:32:07 +0700

    <Exercise M2W4>[Modifiy]: <Modify duplication code>

--- M2_Week_4/tabular_data_analysis/question5.py
+++ M2_Week_4/tabular_data_analysis/question5.py
@@ -1,23 +1,13 @@
 # Question 5
 import numpy as np
 import pandas as pd
-
-def compute_correlation_cofficient (X , Y ):
-    N = len(X)
-    numerator = 0
-    denominator = 0
-    numerator = N * np.sum(X * Y) - np.sum(X) * np.sum(Y)
-    denominator = np.sqrt(N * np.sum(X**2) - np.sum(X)**2) * np.sqrt(N * np.sum(Y**2) - np.sum(Y)**2)
-
-    return np.round(numerator / denominator , 2)
-
+from compute_correlation_cofficient import compute_correlation_cofficient
 
 data = pd.read_csv ("advertising.csv")
-
 x = data ["TV"]
 y = data ["Radio"]
 
-corr_xy = compute_correlation_cofficient (x , y)
+corr_xy = compute_correlation_cofficient(x , y)
 print(f"Correlation between TV and Sales: {round(corr_xy, 2)}")
 
 # Answer: B
